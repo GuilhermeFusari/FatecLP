ade439019452de04325d63b8206252df94c372c2
GuilhermeFusari <guilherme.df000@gmail.com>
2024-04-14 23:55:25 -0300

    Update Forca

--- listas/exercicio5.py
+++ listas/exercicio5.py
@@ -8,19 +8,20 @@ o usuário perderá uma chance. No total, o usuário terá
 """
 import os
 print("---- JOGO DA FORCA----")
-palavra = input("Jogador 1- Digite uma palavra: ")
+palavra = input("Jogador 1- Digite uma palavra: ").lower()
 os.system('cls')
 print("Jogador 2 - Você tem 6 chances para acertar a palavra")
 estado_da_palavra = ["_"]*len(palavra)
 tentativas = 6
 while tentativas !=0:
-    guess = input("Jogador 2 - Digite uma letra: ")
+    guess = input("Jogador 2 - Digite uma letra: ").lower()
+    os.system('cls')
     if guess in palavra:
         print("você acertou uma letra na posição {}".format(palavra.index(guess)))
         for i in range (len(palavra)):
             if guess == palavra[i]:
                 estado_da_palavra[i] = guess
-        print(estado_da_palavra)
+        print(" ".join(estado_da_palavra))
         if ''.join(estado_da_palavra) == palavra:
             os.system('cls')
             print("Voce ganhou, a palavra era "+palavra)
@@ -29,7 +30,7 @@ while tentativas !=0:
     else: 
         tentativas -= 1
         print(f"Tentativas restantes = {tentativas}")
-        print(estado_da_palavra)
+        print(" ".join(estado_da_palavra))
 
     
 if tentativas == 0: 
